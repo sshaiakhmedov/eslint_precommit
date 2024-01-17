26682fa0b59288a0c4727de0382d6d7c0e3f7488
sshaiakhmedov <e.shayakhmedov@gmail.com>
2024-01-16 19:11:49 -0500

    test [re-commit without husky

--- tests/demotest.test.js
+++ tests/demotest.test.js
@@ -13,6 +13,7 @@ test('GET demo endpoint returns successful response', async () => {
   // Add more assertions to verify specific data properties if needed
   expect(data.length).toBeGreaterThan(1) // Example assertion for a hypothetical response
   expect(data.length).not.toBeFalsy() // Example assertion for a hypothetical response
+  expect(data).not.toBeFalsy() // Example assertion for a hypothetical response
 
   expect(data).toHaveLength(5) // Example assertion for a hypothetical response
 
