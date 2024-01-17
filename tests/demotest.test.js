abe4002457b936f13f47b7d3124bbd132c6d1d70
sshaiakhmedov <e.shayakhmedov@gmail.com>
2024-01-16 19:47:50 -0500

    test

--- tests/demotest.test.js
+++ tests/demotest.test.js
@@ -3,7 +3,7 @@ import { expect } from '@jest/globals'
 import { test } from '@jest/globals'
 
 test('GET demo endpoint returns successful response', async () => {
-  const response = await axios.get('https://cat-fact.herokuapp.com/facts/');
+  const response = await axios.get('https://cat-fact.herokuapp.com/facts/')
   expect(response.status).toBe(200) // Assert that the status code is 200
 
   const data = response.data; // Access parsed JSON data directly
